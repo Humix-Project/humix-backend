c5435238e545891c5787797040c45226a0258b7b
Uzrt <worldsilvertree@gmail.com>
2026-06-20 23:18:43 +0900

    벡터 수정 알고리즘 구현

--- src/main/java/com/humix/api/domain/melodyScore/repository/MelodyScoreRepository.java
+++ src/main/java/com/humix/api/domain/melodyScore/repository/MelodyScoreRepository.java
@@ -2,8 +2,6 @@ package com.humix.api.domain.melodyScore.repository;
 
 import com.humix.api.domain.melodyScore.entity.MelodyScore;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 public interface MelodyScoreRepository extends JpaRepository<MelodyScore, Long> {
