64e6a17b02cb60da887e370dd3703c3192020916
Uzrt <worldsilvertree@gmail.com>
2026-06-21 17:13:29 +0900

    벡터화 로직에 로그인 부분 추가

--- src/main/java/com/humix/api/global/security/config/SecurityConfig.java
+++ src/main/java/com/humix/api/global/security/config/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig {
 
                         // 🚨 특정 도메인은 무조건 인증(로그인) 필요하게 막기
                         .requestMatchers("/api/v1/upload/**").authenticated()
+                        .requestMatchers("/api/v1/hummings/**").authenticated()
 
                         // 나머지는 다 인증 불필요
                         .anyRequest().permitAll()
